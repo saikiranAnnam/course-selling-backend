ad966d7b6cd7f5ed287435c6c870c32612bb99d6
saikiranAnnam <saikiranannam23@gmail.com>
2024-01-16 16:31:32 +0530

    implemented signIn auth router

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { Admin, Course } = require('../db');
+const { Admin, Course, User } = require('../db');
+const jwt = require('jsonwebtoken');
 const AdminMiddleware = require('../middleware/admin');
 const router = Router();
-
+const JWT_SECERT = "12345"
 
 router.post('/signup', async(req, res) =>{
     // admins signup logic
@@ -25,9 +26,32 @@ router.post('/signup', async(req, res) =>{
     }
 });
 
-router.post('/signin', AdminMiddleware, (req, res) =>{
+router.post('/signin', (req, res) =>{
     // admin signIn logic
+    try{
+        const username = req.headers.username;
+        const password = req.headers.password;
 
+        const user = User.find({
+            username, 
+            password
+        })
+        if(user){
+            const token = jwt.sign({username: username}, JWT_SECERT);
+            res.json({
+                token
+            })
+        }else{
+            res.json({
+                message : "Admin not found !!!"
+            })
+        }
+    }catch(error){
+        res.json({
+            message : error.message
+        })
+    }
+    
 });
 
 router.post('/courses', AdminMiddleware, (req, res) =>{
